ca91a00e2922c06365efc082b8d255e7276322b9
saroar <saroar9@gmail.com>
2020-11-12 14:29:14 +0300

    add conversation create functions to gatway app

--- Sources/App/API/Chat/ChatsAPI.swift
+++ Sources/App/API/Chat/ChatsAPI.swift
@@ -8,218 +8,239 @@
 import Vapor
 
 public struct ChatsAPI {
-    let application: Application
-
-    public init (_ app: Application) {
-        application = app
-    }
+  let application: Application
+  
+  public init (_ app: Application) {
+    application = app
+  }
 }
 
 // MARK: - Configuration
 
 extension ChatsAPI {
-    struct ConfigurationKey: StorageKey {
-        typealias Value = ChatsAPIConfiguration
+  struct ConfigurationKey: StorageKey {
+    typealias Value = ChatsAPIConfiguration
+  }
+  
+  public var configuration: ChatsAPIConfiguration? {
+    get {
+      application.storage[ConfigurationKey.self]
     }
-
-    public var configuration: ChatsAPIConfiguration? {
-        get {
-            application.storage[ConfigurationKey.self]
-        }
-        nonmutating set {
-            application.storage[ConfigurationKey.self] = newValue
-        }
+    nonmutating set {
+      application.storage[ConfigurationKey.self] = newValue
     }
+  }
 }
 
 // MARK: Send message
 
 extension ChatsAPI {
-    // WebSokcet request
-    func chat(_ req: Request) throws -> EventLoopFuture<ClientResponse> {
-        guard let configuration = self.configuration else {
-            fatalError("ChatsAPI not configured. Use app.chats.configuration = ...")
-        }
-        guard let token = req.headers[.authorization].first else {
-            throw Abort(.badRequest, reason: "No token")
-        }
-
-        var uri = configuration.baseURL
-        uri.path = "\(req.url)/chat"
-        return application.client.get(uri, headers: [
-            "Content-Type":"application/json",
-            "authorization": token
-        ])
-        
+  // WebSokcet request
+  func chat(_ req: Request) throws -> EventLoopFuture<ClientResponse> {
+    guard let configuration = self.configuration else {
+      fatalError("ChatsAPI not configured. Use app.chats.configuration = ...")
+    }
+    guard let token = req.headers[.authorization].first else {
+      throw Abort(.badRequest, reason: "No token")
     }
     
-    // For Message controller
-    func readAllMessagesByConversationID(_ req: Request) throws -> EventLoopFuture<ClientResponse> {
-
-        guard let configuration = self.configuration else {
-            fatalError("ChatsAPI not configured. Use app.chats.configuration = ...")
-        }
-
-        guard let token = req.headers[.authorization].first else {
-            throw Abort(.badRequest, reason: "No token")
-        }
-
-        var uri = configuration.baseURL
-        guard let conversationsId = req.parameters.get("conversationsId") else {
-            throw Abort(.badRequest, reason: "param id is missing")
-        }
-        
-        uri.path += "/messages/by/conversations/\(conversationsId)"
-        return application.client.get(uri, headers: [
-            "Content-Type":"application/json",
-            "authorization": token
-        ])
+    var uri = configuration.baseURL
+    uri.path = "\(req.url)/chat"
+    return application.client.get(uri, headers: [
+      "Content-Type":"application/json",
+      "authorization": token
+    ])
+    
+  }
+  
+  // For Message controller
+  func createConversation(_ req: Request) throws -> EventLoopFuture<ClientResponse> {
+    guard let configuration = self.configuration else {
+      fatalError("ChatsAPI not configured. Use app.chats.configuration = ...")
     }
     
-    func readAllConversations(_ req: Request) throws -> EventLoopFuture<ClientResponse> {
-
-        guard let configuration = self.configuration else {
-            fatalError("ChatsAPI not configured. Use app.chats.configuration = ...")
-        }
-
-        guard let token = req.headers[.authorization].first else {
-            throw Abort(.badRequest, reason: "No token")
-        }
-
-        var uri = configuration.baseURL
-        uri.path += "/conversations"
-        return application.client.get(uri, headers: [
-            "Content-Type":"application/json",
-            "authorization": token
-        ])
+    guard let token = req.headers[.authorization].first else {
+      throw Abort(.badRequest, reason: "No token")
     }
     
-    func readAllMessageByCoversationID(_ req: Request) throws -> EventLoopFuture<ClientResponse> {
-
-        guard let configuration = self.configuration else {
-            fatalError("ChatsAPI not configured. Use app.chats.configuration = ...")
-        }
-
-        guard let token = req.headers[.authorization].first else {
-            throw Abort(.badRequest, reason: "No token")
-        }
-
-        var uri = configuration.baseURL
-        guard let conversationsId = req.parameters.get("conversationsId") else {
-            throw Abort(.badRequest, reason: "param id is missing")
-        }
-        
-        uri.path += "/conversations/\(conversationsId)/messages"
-        return application.client.get(uri, headers: [
-            "Content-Type":"application/json",
-            "authorization": token
-        ])
-    }
-
-    func addUserToConversation(_ req: Request) throws -> EventLoopFuture<ClientResponse> {
-
-        guard let configuration = self.configuration else {
-            fatalError("ChatsAPI not configured. Use app.chats.configuration = ...")
-        }
-
-        guard let token = req.headers[.authorization].first else {
-            throw Abort(.badRequest, reason: "Missing Authorization token")
-        }
-
-        var uri = configuration.baseURL
-
-        guard let conversationsId = req.parameters.get("conversationsId"),
-              let usersId = req.parameters.get("usersId") else {
-            throw Abort(.badRequest, reason: "param conversationsId/usersId is missing")
-        }
-
-        uri.path += "/conversations/\(conversationsId)/users/\(usersId)"
-
-        return application.client.post(uri, headers: [
-            "Content-Type":"application/json",
-            "authorization": token
-        ], beforeSend: { outGoingReq in
-            outGoingReq.body = req.body.data
-        })
-    }
-
-    func updateMessage(_ req: Request) throws -> EventLoopFuture<ClientResponse> {
-
-        guard let configuration = self.configuration else {
-            fatalError("ChatsAPI not configured. Use app.chats.configuration = ...")
-        }
-
-        guard let token = req.headers[.authorization].first else {
-            throw Abort(.badRequest, reason: "No token")
-        }
-
-        var uri = configuration.baseURL
-        uri.path += "/messages"
-        return application.client.put(uri, headers: [
-            "Content-Type":"application/json",
-            "authorization": token
-        ])
+    var uri = configuration.baseURL
+    uri.path += "/conversations"
+    
+    return application.client.post(uri, headers: [
+      "Content-Type":"application/json",
+      "authorization": token
+    ], beforeSend: { outGoingReq in
+      outGoingReq.body = req.body.data
+    })
+    
+  }
+  
+  func readAllMessagesByConversationID(_ req: Request) throws -> EventLoopFuture<ClientResponse> {
+    
+    guard let configuration = self.configuration else {
+      fatalError("ChatsAPI not configured. Use app.chats.configuration = ...")
     }
     
-    func updateConversations(_ req: Request) throws -> EventLoopFuture<ClientResponse> {
-
-        guard let configuration = self.configuration else {
-            fatalError("ChatsAPI not configured. Use app.chats.configuration = ...")
-        }
-
-        guard let token = req.headers[.authorization].first else {
-            throw Abort(.badRequest, reason: "No token")
-        }
-
-        var uri = configuration.baseURL
-        uri.path += "/conversations"
-        return application.client.put(uri, headers: [
-            "Content-Type":"application/json",
-            "authorization": token
-        ])
+    guard let token = req.headers[.authorization].first else {
+      throw Abort(.badRequest, reason: "No token")
     }
     
-    func deleteConversation(_ req: Request) throws -> EventLoopFuture<ClientResponse> {
-        guard let configuration = self.configuration else {
-            fatalError("ChatsAPI not configured. Use app.chats.configuration = ...")
-        }
-
-        guard let token = req.headers[.authorization].first else {
-            throw Abort(.badRequest, reason: "No token")
-        }
-
-        var uri = configuration.baseURL
-        guard let conversationsId = req.parameters.get("conversationsId") else {
-            throw Abort(.badRequest, reason: "param id is missing")
-        }
-        uri.path += "/conversations/\(conversationsId)"
-
-        return application.client.delete(uri, headers: [
-            "Content-Type":"application/json",
-            "authorization": token
-        ])
+    var uri = configuration.baseURL
+    guard let conversationsId = req.parameters.get("conversationsId") else {
+      throw Abort(.badRequest, reason: "param id is missing")
     }
-
-    func deleteMessage(_ req: Request) throws -> EventLoopFuture<ClientResponse> {
-        guard let configuration = self.configuration else {
-            fatalError("ChatsAPI not configured. Use app.chats.configuration = ...")
-        }
-
-        guard let token = req.headers[.authorization].first else {
-            throw Abort(.badRequest, reason: "No token")
-        }
-
-        var uri = configuration.baseURL
-        guard let messagesId = req.parameters.get("messagesId") else {
-            throw Abort(.badRequest, reason: "param id is missing")
-        }
-        uri.path += "/messages/\(messagesId)"
-
-        return application.client.delete(uri, headers: [
-            "Content-Type":"application/json",
-            "authorization": token
-        ])
+    
+    uri.path += "/messages/by/conversations/\(conversationsId)"
+    return application.client.get(uri, headers: [
+      "Content-Type":"application/json",
+      "authorization": token
+    ])
+  }
+  
+  func readAllConversations(_ req: Request) throws -> EventLoopFuture<ClientResponse> {
+    
+    guard let configuration = self.configuration else {
+      fatalError("ChatsAPI not configured. Use app.chats.configuration = ...")
     }
+    
+    guard let token = req.headers[.authorization].first else {
+      throw Abort(.badRequest, reason: "No token")
+    }
+    
+    var uri = configuration.baseURL
+    uri.path += "/conversations"
+    return application.client.get(uri, headers: [
+      "Content-Type":"application/json",
+      "authorization": token
+    ])
+  }
+  
+  func readAllMessageByCoversationID(_ req: Request) throws -> EventLoopFuture<ClientResponse> {
+    
+    guard let configuration = self.configuration else {
+      fatalError("ChatsAPI not configured. Use app.chats.configuration = ...")
+    }
+    
+    guard let token = req.headers[.authorization].first else {
+      throw Abort(.badRequest, reason: "No token")
+    }
+    
+    var uri = configuration.baseURL
+    guard let conversationsId = req.parameters.get("conversationsId") else {
+      throw Abort(.badRequest, reason: "param id is missing")
+    }
+    
+    uri.path += "/conversations/\(conversationsId)/messages"
+    return application.client.get(uri, headers: [
+      "Content-Type":"application/json",
+      "authorization": token
+    ])
+  }
+  
+  func addUserToConversation(_ req: Request) throws -> EventLoopFuture<ClientResponse> {
+    
+    guard let configuration = self.configuration else {
+      fatalError("ChatsAPI not configured. Use app.chats.configuration = ...")
+    }
+    
+    guard let token = req.headers[.authorization].first else {
+      throw Abort(.badRequest, reason: "Missing Authorization token")
+    }
+    
+    var uri = configuration.baseURL
+    
+    guard let conversationsId = req.parameters.get("conversationsId"),
+          let usersId = req.parameters.get("usersId") else {
+      throw Abort(.badRequest, reason: "param conversationsId/usersId is missing")
+    }
+    
+    uri.path += "/conversations/\(conversationsId)/users/\(usersId)"
+    
+    return application.client.post(uri, headers: [
+      "Content-Type":"application/json",
+      "authorization": token
+    ], beforeSend: { outGoingReq in
+      outGoingReq.body = req.body.data
+    })
+  }
+  
+  func updateMessage(_ req: Request) throws -> EventLoopFuture<ClientResponse> {
+    
+    guard let configuration = self.configuration else {
+      fatalError("ChatsAPI not configured. Use app.chats.configuration = ...")
+    }
+    
+    guard let token = req.headers[.authorization].first else {
+      throw Abort(.badRequest, reason: "No token")
+    }
+    
+    var uri = configuration.baseURL
+    uri.path += "/messages"
+    return application.client.put(uri, headers: [
+      "Content-Type":"application/json",
+      "authorization": token
+    ])
+  }
+  
+  func updateConversations(_ req: Request) throws -> EventLoopFuture<ClientResponse> {
+    
+    guard let configuration = self.configuration else {
+      fatalError("ChatsAPI not configured. Use app.chats.configuration = ...")
+    }
+    
+    guard let token = req.headers[.authorization].first else {
+      throw Abort(.badRequest, reason: "No token")
+    }
+    
+    var uri = configuration.baseURL
+    uri.path += "/conversations"
+    return application.client.put(uri, headers: [
+      "Content-Type":"application/json",
+      "authorization": token
+    ])
+  }
+  
+  func deleteConversation(_ req: Request) throws -> EventLoopFuture<ClientResponse> {
+    guard let configuration = self.configuration else {
+      fatalError("ChatsAPI not configured. Use app.chats.configuration = ...")
+    }
+    
+    guard let token = req.headers[.authorization].first else {
+      throw Abort(.badRequest, reason: "No token")
+    }
+    
+    var uri = configuration.baseURL
+    guard let conversationsId = req.parameters.get("conversationsId") else {
+      throw Abort(.badRequest, reason: "param id is missing")
+    }
+    uri.path += "/conversations/\(conversationsId)"
+    
+    return application.client.delete(uri, headers: [
+      "Content-Type":"application/json",
+      "authorization": token
+    ])
+  }
+  
+  func deleteMessage(_ req: Request) throws -> EventLoopFuture<ClientResponse> {
+    guard let configuration = self.configuration else {
+      fatalError("ChatsAPI not configured. Use app.chats.configuration = ...")
+    }
+    
+    guard let token = req.headers[.authorization].first else {
+      throw Abort(.badRequest, reason: "No token")
+    }
+    
+    var uri = configuration.baseURL
+    guard let messagesId = req.parameters.get("messagesId") else {
+      throw Abort(.badRequest, reason: "param id is missing")
+    }
+    uri.path += "/messages/\(messagesId)"
+    
+    return application.client.delete(uri, headers: [
+      "Content-Type":"application/json",
+      "authorization": token
+    ])
+  }
 }
 
 
--- Sources/App/Controllers/ConversationController.swift
+++ Sources/App/Controllers/ConversationController.swift
@@ -8,51 +8,57 @@
 import Vapor
 
 extension ConversationController: RouteCollection {
-    func boot(routes: RoutesBuilder) throws {
-        routes.post(":conversationsId", "users", ":usersId", use: addUserToConversation)
-        routes.get(use: readAll) // "users", ":users_id",
-        routes.get(":conversationsId", "messages", use: readAllMessageByCoversationID)
-        routes.put(use: update)
-        routes.delete(":conversationsId", use: delete)
-    }
+  func boot(routes: RoutesBuilder) throws {
+    routes.post(use: create)
+    routes.post(":conversationsId", "users", ":usersId", use: addUserToConversation)
+    routes.get(use: readAll) // "users", ":users_id",
+    routes.get(":conversationsId", "messages", use: readAllMessageByCoversationID)
+    routes.put(use: update)
+    routes.delete(":conversationsId", use: delete)
+  }
 }
 
 final class ConversationController {
+  
+  func create(_ req: Request) throws -> EventLoopFuture<ClientResponse> {
+    if req.loggedIn == false { throw Abort(.unauthorized) }
+    return try req.chats.createConversation(req).hop(to: req.eventLoop)
+  }
+  
+  func readAll(_ req: Request) throws -> EventLoopFuture<ClientResponse> {
     
-    func readAll(_ req: Request) throws -> EventLoopFuture<ClientResponse> {
-        
-        if req.loggedIn == false { throw Abort(.unauthorized) }
-        
-        return try req.chats.readAllConversations(req).hop(to: req.eventLoop)
+    if req.loggedIn == false { throw Abort(.unauthorized) }
+    
+    return try req.chats.readAllConversations(req).hop(to: req.eventLoop)
+  }
+  
+  private func readAllMessageByCoversationID(_ req: Request) throws -> EventLoopFuture<ClientResponse> {
+    
+    if req.loggedIn == false { throw Abort(.unauthorized) }
+    
+    return try req.chats.readAllMessageByCoversationID(req).hop(to: req.eventLoop)
+    
+  }
+  
+  func addUserToConversation(_ req: Request) throws -> EventLoopFuture<ClientResponse> {
+    return try req.chats.addUserToConversation(req).hop(to: req.eventLoop)
+  }
+  
+  private func update(_ req: Request) throws -> EventLoopFuture<ClientResponse> {
+    if req.loggedIn == false {
+      throw Abort(.unauthorized)
     }
     
-    private func readAllMessageByCoversationID(_ req: Request) throws -> EventLoopFuture<ClientResponse> {
-        
-        if req.loggedIn == false { throw Abort(.unauthorized) }
-        
-        return try req.chats.readAllMessageByCoversationID(req).hop(to: req.eventLoop)
-        
-    }
-
-    func addUserToConversation(_ req: Request) throws -> EventLoopFuture<ClientResponse> {
-      return try req.chats.addUserToConversation(req).hop(to: req.eventLoop)
-    }
-
-    private func update(_ req: Request) throws -> EventLoopFuture<ClientResponse> {
-        if req.loggedIn == false {
-            throw Abort(.unauthorized)
-        }
-
-        return try req.chats.updateConversations(req).hop(to: req.eventLoop)
-    }
-
-    private func delete(_ req: Request) throws -> EventLoopFuture<ClientResponse> {
-        if req.loggedIn == false { throw Abort(.unauthorized) }
-
-        return try req.chats.deleteConversation(req).hop(to: req.eventLoop)
-        
-    }
-
+    return try req.chats.updateConversations(req).hop(to: req.eventLoop)
+  }
+  
+  private func delete(_ req: Request) throws -> EventLoopFuture<ClientResponse> {
+    if req.loggedIn == false { throw Abort(.unauthorized) }
+    
+    return try req.chats.deleteConversation(req).hop(to: req.eventLoop)
+    
+  }
+  
 }
 
 
